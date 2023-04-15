9d397b6d13ba9d26e1798927f55202d4223ba046
unknown <Ivan>
2023-04-16 01:38:22 +0700

    ready task_13

--- home_work_02/task_13/Program.cs
+++ home_work_02/task_13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number >= 1000){
+    Console.WriteLine("Третьей цифры нет!");
+}  else{
+    int Checking3rdDigit (int num)
+    {
+    int chek = num % 10;
+    return chek;
+    }
+    Console.WriteLine($"Третья цифра заданного числа --> {Checking3rdDigit(number)}");
+}
+
+
+
+ 
+
+
+
