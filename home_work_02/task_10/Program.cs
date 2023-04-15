5f670554f542fad175aee623770573ce1c3d1071
unknown <Ivan>
2023-04-16 00:50:28 +0700

    ready task_10

--- home_work_02/task_10/Program.cs
+++ home_work_02/task_10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindCenter (int num)
+{
+    int digitCenter = (num / 10) % 10;
+    return digitCenter;
+}
+Console.WriteLine($"Вторая цифра числа {number} --> {FindCenter(number)}");
